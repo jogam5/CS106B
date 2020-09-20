7db1352e1ebca8b0c8f6f2bb83ba14200d782466
jogam5 <jogam5@yahoo.com.mx>
2020-09-19 20:42:03 -0500

    solved combinatorics problem

--- assign3-starter/src/combos.cpp
+++ assign3-starter/src/combos.cpp
@@ -11,6 +11,7 @@
 #include "testing/SimpleTest.h"
 using namespace std;
 
+
 // Returns the number of ways to choose k things from a set of size N
 // using the formula based on factorial. Because an int type can only hold
 // value of factorial up to n = 12, this formula only works correctly for
@@ -20,12 +21,26 @@ int combo_formula(int n, int k)
     return factorial(n)/(factorial(k)*factorial(n-k));
 }
 
-// TODO: Add a function header comment here to explain the
-// behavior of the function and how you implemented this behavior
+// Returns the number of ways to choose k different elements from a set
+// of size n by using a recursive formula.
 int count_combos(int n, int k)
 {
-    //TODO your code here
-    return 0;
+    if (k == 0) {
+        return 1;
+    }
+    if (k == n) {
+        return 1;
+    }
+    if (n < 0) {
+        error("The number of n must be positive.");
+    }
+    if (k < 0) {
+        error("The number of k must be positive.");
+    }
+    if (n < k) {
+        error("n must be greater or equal to k");
+    }
+    return count_combos(n-1, k-1) + count_combos(n-1, k);
 }
 
 
@@ -34,7 +49,20 @@ int count_combos(int n, int k)
 PROVIDED_TEST("Compare count_combos to combo_formula") {
     for (int i = 0; i < 25; i++) {
         int n = randomInteger(1, 12);
+        //cout << "n: " << n << endl;
         int k = randomInteger(1, n);
+        //cout << "k: " << k << endl;
         EXPECT_EQUAL(count_combos(n, k), combo_formula(n, k));
     }
 }
+
+STUDENT_TEST("Combinations of n greaterthan 12") {
+        EXPECT_EQUAL(455, count_combos(15,3));
+}
+
+STUDENT_TEST("Check for different errors") {
+    EXPECT_ERROR(count_combos(3, 10));
+    EXPECT_ERROR(count_combos(7, -3));
+    EXPECT_ERROR(count_combos(-7, 1));
+}
+
