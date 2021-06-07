5b58eb95990ace0272ff9fefd6ec9069501edce7
jogam5 <jogam5@yahoo.com.mx>
2021-06-07 13:31:46 -0500

    improved isMeasurable

--- extra/backtrackingSection3.cpp
+++ extra/backtrackingSection3.cpp
@@ -1,76 +1,4 @@
 /*
- * Exercise 1, Win some, lose sum.
- *
-
-#include <iostream>
-#include "testing/SimpleTest.h"
-#include "testing/TextUtils.h"
-#include "vector.h"
-using namespace std;
-
-
-/*
- * sumHelper reads in a vector and recursively compares 'target'
- * versus 'sum'. The core of the function lies in the backtracking
- * portion where we explore two possible branches following the
- * framework:
- *  Choose
- *  Recur
- *  Unchoose
- */
-bool sumHelper(Vector<int>& values, int target, int sum) {
-    if (values.isEmpty()) {
-        /* Base case
-         * Notes:
-         * 1. "target == sum" is equivalent to first checking if
-         * the comparision is true and later returning bool value.
-         */
-        return target == sum;
-    } else {
-        /* Recursive case
-         * Notes:
-         * 1. In the short circuit evaluation "||", if the first call
-         * to sumHelper is true, there's no need to check for the
-         * second call.
-         */
-        int firstElement = values[0];
-        values.remove(0); // Remove the first element or 'Choose'
-        bool answer = sumHelper(values, target, sum + firstElement) || sumHelper(values, target, sum); // Recur or Explore
-        values.insert(0, firstElement); // Insert back the first element or Unchoose
-        return answer;
-    }
-    return false;
-}
-
-
-/*
- * Win some, lose sum (Code Writing)
- * --------------------------------------------------------
- * Write a recursive function named canMakeSum that takes a
- * reference to a Vector<int> and an int target value and
- * returns true if it is possible to have some selection of
- * values from the Vector that sum to the target value.
- */
-bool canMakeSum(Vector<int>& values, int target) {
-    return sumHelper(values, target, 0);
-}
-
-
-
-/* * * * * Provided Tests Below This Point * * * * */
-
-PROVIDED_TEST("Provided Test: Positive example from handout.") {
-    Vector<int> nums = {1,1,2,3,5};
-    EXPECT(canMakeSum(nums, 9));
-}
-
-PROVIDED_TEST("Provided Test: Negative example from handout"){
-    Vector<int> nums = {1,4,5,6};
-    EXPECT(!canMakeSum(nums, 8));
-}
-
-
-/*
  * CS106B Section Handout Test Harness: Section 3
  * ----------------------------------------------
  * These problems have been galvanized from years of
@@ -103,6 +31,9 @@ using namespace std;
  * Things to consider:
  * 1. Ask yourself what do you need to acumulate
  * 2. Backtracking structure: choose, recurse, unchoose
+ * 3. In the case B it's not a whole sum, it could be a partial sum. Thus
+ * probably is much more similar to what we have done before. Try sketching
+ * a tree.
  */
 
 bool isMeasurable(int target, Vector<int>& weights, int sum) {
@@ -120,25 +51,48 @@ bool isMeasurable(int target, Vector<int>& weights, int sum) {
 
 }
 
-bool recurse(int target, Vector<int>& weights, int sum) {
+// To do:
+// Refactor, Join Methods, Run tree to understand || short circuit operator, Understand when Unchoose
+bool isSum(int sum, int target, Vector<int> v) {
+    if (v.isEmpty()) return false;
+    if (sum == target + v[0]) {
+        cout << "sum: " << sum << ", target: " << target << ", v[0]: " << v[0] << endl;
+        return true;
+     }
+
+    else {
+        v.remove(0);
+        return isSum(sum, target, v);
+    }
+}
+
+bool recurse(int target, Vector<int>& weights, int sum, Vector<int> original) {
     if (weights.isEmpty()) {
         // Base case
-        return target == sum;
+        //return target == sum;
+        cout << "sum: " << sum << ", target: " << target << endl;
+        return isSum(sum, target, original);
     } else {
         // Recursive case
         int n = weights[0]; // choose
         weights.remove(0);
-        int acc = accumulate(weights.begin(),weights.end(),0);
-        bool result = isMeasurable(n + target, weights, acc);
+
+        //int acc = accumulate(weights.begin(),weights.end(),0);
+        //cout << "sum entering recurse: " << sum + n << endl;
+        bool result = recurse(target, weights, sum + n, original) || recurse(target, weights, sum, original);
+        //recurse(target, weights, sum + n, original);
+        //recurse(target, weights, sum, original);
         weights.add(n); // unchoose
         return result;
     }
+    //return false;
 }
 
 
 bool isMeasurable(int target, Vector<int>& weights){
     //return isMeasurable(target, weights, 0);
-    return recurse(target, weights, 0);
+    Vector<int> original = weights;
+    return recurse(target, weights, 0, original);
 }
 
 
@@ -178,3 +132,4 @@ PROVIDED_TEST("Provided Test: Complex Positive Example"){
     Vector<int> weights = {1, 3, 7};
     EXPECT(isMeasurable(6, weights));
 }
+
